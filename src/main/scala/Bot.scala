c297a50df7de25290532d80334045af29e852ad5
SammysHP <sven@sammyshp.de>
2013-01-08 20:47:26 +0100

    Add command line arguments
    -s server
    -c channel
    -n name

--- src/main/scala/Bot.scala
+++ src/main/scala/Bot.scala
@@ -1,21 +1,33 @@
 import org.pircbotx.PircBotX
 
 object Bot extends App {
-  val NAME = "cgeo-irct"
-  val SERVER = "irc.freenode.net"
-  val CHANNEL = "#cgeo-test"
+  val DEFAULT_NAME = "cgeo-irct"
+  val DEFAULT_SERVER = "irc.freenode.net"
+  val DEFAULT_CHANNEL = "#cgeo"
+
+  def parseOptions(args: List[String], options: Map[String, String]): Map[String, String] = {
+    args match {
+      case Nil => options
+      case "-s" :: server :: tail => parseOptions(tail, options + ("server" -> server))
+      case "-c" :: channel :: tail => parseOptions(tail, options + ("channel" -> channel))
+      case "-n" :: name :: tail => parseOptions(tail, options + ("name" -> name))
+      case _ :: tail => parseOptions(tail, options)
+    }
+  }
+
+  val options = parseOptions(args.toList, Map())
 
   try {
     val bot = new PircBotX
     bot.getListenerManager().addListener(IssueLinker)
     bot.getListenerManager().addListener(Admin)
     bot.getListenerManager().addListener(Help)
-    bot.setName(NAME)
+    bot.setName(options.getOrElse("name", DEFAULT_NAME))
     bot.setAutoNickChange(true)
 
     print("[info] Connecting... ")
-    bot.connect(SERVER)
-    bot.joinChannel(CHANNEL)
+    bot.connect(options.getOrElse("server", DEFAULT_SERVER))
+    bot.joinChannel(options.getOrElse("channel", DEFAULT_CHANNEL))
     println("ok!")
   } catch {
     case e: Exception => println("ERROR: " + e)
