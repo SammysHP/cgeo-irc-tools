b44679caca0a7105e83bad2ec67c3cb104f522f7
SammysHP <sven@sammyshp.de>
2013-01-08 12:35:38 +0100

    New: change password on runtime
    Default password is "admin"

--- src/main/scala/tools/Admin.scala
+++ src/main/scala/tools/Admin.scala
@@ -5,16 +5,25 @@ import org.pircbotx.hooks.events.PrivateMessageEvent
 object Admin extends ListenerAdapter[PircBotX] {
   type Message = PrivateMessageEvent[PircBotX]
 
-  val ADMIN_PW = "dothatshit"
+  var ADMIN_PW = "admin"
 
   override def onPrivateMessage(event: Message) = {
     (event.getMessage().split(" ").toList: @unchecked) match {
-      case "admin" :: ADMIN_PW :: command => {
-        (command: @unchecked) match {
-          case "shutdown" :: _ => {
-            event.respond("Shutdown bot...")
-            println("[info] Request shutdown")
-            event.getBot().shutdown()
+      case "admin" :: password :: command => {
+        if (ADMIN_PW != password) {
+          event.respond("Wrong password!")
+        } else {
+          command match {
+            case "shutdown" :: _ => {
+              event.respond("Shutting down bot...")
+              println("[info] Shutdown requested")
+              event.getBot().shutdown()
+            }
+            case "setpw" :: password :: _ => {
+              ADMIN_PW = password
+              event.respond("Password set to \"" + password + "\"")
+            }
+            case _ => event.respond("Unknown command, try 'help'")
           }
         }
       }
