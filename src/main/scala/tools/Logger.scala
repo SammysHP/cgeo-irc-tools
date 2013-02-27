7291188dde11cdd8daf130517da65c91186aed6b
SammysHP <sven@sammyshp.de>
2013-02-27 22:22:39 +0100

    Add logging for join/part

--- src/main/scala/tools/Logger.scala
+++ src/main/scala/tools/Logger.scala
@@ -1,6 +1,6 @@
 import org.pircbotx.PircBotX
 import org.pircbotx.hooks.ListenerAdapter
-import org.pircbotx.hooks.events.{ActionEvent, MessageEvent}
+import org.pircbotx.hooks.events.{ActionEvent, JoinEvent, MessageEvent, PartEvent}
 
 import java.io.FileWriter
 import java.util.Date
@@ -11,22 +11,26 @@ class Logger(path: String) extends ListenerAdapter[PircBotX] {
   val logDate = new java.text.SimpleDateFormat("HH:mm:ss")
 
   override def onMessage(event: MessageEvent[PircBotX]) = {
-    logMessage(event.getUser.getNick, event.getMessage)
+    logMessage(event.getUser.getNick + ":", event.getMessage)
   }
 
   override def onAction(event: ActionEvent[PircBotX]) = {
-    logMessage(event.getUser.getNick, "***" + event.getUser.getNick + " " + event.getMessage)
+    logMessage(event.getUser.getNick + ":", "***" + event.getUser.getNick + " " + event.getMessage)
+  }
+
+  override def onJoin(event: JoinEvent[PircBotX]) = {
+    logMessage(">>>", event.getUser.getNick + " joined the channel")
+  }
+
+  override def onPart(event: PartEvent[PircBotX]) = {
+    logMessage("<<<", event.getUser.getNick + " left the channel")
   }
 
   private def logMessage(user: String, message: String) = {
     try {
       val file = new FileWriter(path + "/" + fileDate.format(new Date()) + ".txt", true)
 
-      file.write("%-10s%-15s %s%n".format(
-        logDate.format(new Date()),
-        user + ":",
-        message
-      ))
+      file.write("%-10s%-15s %s%n".format(logDate.format(new Date()), user, message))
 
       file.close
     } catch {
